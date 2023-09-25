6435b12768e10e102b7d158deac82c6c9386959d
wokron <stringcatwok@gmail.com>
2023-09-25 15:58:33 +0800

    feat: parser exception

---
+++ src/java/sysy/exception/ParserException.java
@@ -0,0 +1,11 @@
+package sysy.exception;
+
+public class ParserException extends CompilerException {
+    public ParserException() {
+        super();
+    }
+
+    public ParserException(String message) {
+        super(message);
+    }
+}
