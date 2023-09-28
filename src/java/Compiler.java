d5b32612499fb0ca2fafcef073dfe4521343e677
wokron <stringcatwok@gmail.com>
2023-09-28 15:54:42 +0800

    refactor: modify structure of main

--- src/java/Compiler.java
+++ src/java/Compiler.java
@@ -7,18 +7,41 @@ import java.io.*;
 
 public class Compiler {
     public static void main(String[] args) throws IOException, LexerException, ParserException {
-        try (var in = new FileInputStream("testfile.txt");
-             var out = new FileOutputStream("output.txt")) {
-            var output = new PrintStream(out);
-            var lexer = new Lexer(new InputStreamReader(in));
+//        task1();
+        task2();
+    }
+
+    private static void task1() throws IOException, LexerException {
+        try (var testFile = new FileInputStream("testfile.txt");
+             var outputFile = new FileOutputStream("output.txt")) {
+            var out = new PrintStream(outputFile);
+            var lexer = new Lexer(new InputStreamReader(testFile));
+            while (lexer.next()) {
+                var token = lexer.getToken();
+                out.printf("%s %s\n", token.getType().name(), token.getValue());
+            }
+        }
+    }
+
+    private static void task2() throws IOException, LexerException, ParserException {
+        try (var testFile = new FileInputStream("testfile.txt");
+             var outputFile = new FileOutputStream("output.txt")) {
+            var out = new PrintStream(outputFile);
+            var lexer = new Lexer(new InputStreamReader(testFile));
             var parser = new Parser(lexer);
             var result = parser.parse();
-            System.out.println(result);
-            result.walk(output::println, output::println);
-//            while (lexer.next()) {
-//                var token = lexer.getToken();
-//                output.printf("%s %s %d\n", token.getType().name(), token.getValue(), token.getLineNum());
-//            }
+            result.walk(
+                    out::println,
+                    nonTerminalSymbol -> {
+                        String type = nonTerminalSymbol.getType();
+                        if (!type.equals("BlockItem")
+                                && !type.equals("Decl")
+                                && !type.equals("BType")
+                        ) {
+                            out.println(nonTerminalSymbol);
+                        }
+                    }
+            );
         }
     }
 }
